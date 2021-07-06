3f22eea3dd56b11def0f96a00ba0d7bcf3e6f468
longdog <linda@longdog.us>
2021-07-06 13:22:26 +0400

    add pattern 3

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,5 +6,3 @@ find ./src/patterns/ -type f -print0 | while IFS= read -r -d $'\0' file;
   base=${name%.txt}
   node ../koginlib/cli/index.js --pattern="${file}" --output="./src/img/${base}.png"
 done
-        
-        #node ../koginlib/cli/index.js --pattern="./src/pattents/$filename" --output="./src/img/$(basename "$filename" .txt).png"
